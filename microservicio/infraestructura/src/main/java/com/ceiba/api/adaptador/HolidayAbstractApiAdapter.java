5a0e680e92748c60946883157957a872e284c5ed
daniel.parra <josedanielparra05@gmail.com>
2020-12-26 00:27:19 -0500

    Solucion code smells  y covertura

--- microservicio/infraestructura/src/main/java/com/ceiba/api/adaptador/HolidayAbstractApiAdapter.java
+++ microservicio/infraestructura/src/main/java/com/ceiba/api/adaptador/HolidayAbstractApiAdapter.java
@@ -3,17 +3,26 @@ package com.ceiba.api.adaptador;
 import java.io.IOException;
 import java.time.LocalDateTime;
 
+import org.apache.http.client.fluent.Content;
+import org.apache.http.client.fluent.Request;
 import org.springframework.stereotype.Component;
 
-import com.ceiba.api.consumer.HolidayAbstractApiConsumer;
 import com.ceiba.tiqueteparqueo.puerto.api.ApiValidadorFechaFestivo;
 
 @Component
 public class HolidayAbstractApiAdapter implements ApiValidadorFechaFestivo{
 
+	private static final String PAIS = "CO";
+	private static final String API_KEY = "5e0e2b64c9964607bac699cc84e42f74";
+	private static final String END_POINT = "https://holidays.abstractapi.com/v1/?";
+	
 	@Override
 	public boolean esFestivo(LocalDateTime fecha) throws IOException {
-		return HolidayAbstractApiConsumer.esFestivo(fecha);
+		Content content = Request.Get(END_POINT + "api_key=+" + API_KEY + "&country=" + PAIS + "&year="
+				+ fecha.getYear() + "&month=" + fecha.getMonthValue() + "&day=" + fecha.getDayOfMonth()).execute()
+				.returnContent();
+
+		return !"[]".equals(content.toString());
 	}
 	
 }
--- microservicio/infraestructura/src/test/java/com/ceiba/api/HolidayAbstractApiTest.java
+++ microservicio/infraestructura/src/test/java/com/ceiba/api/HolidayAbstractApiTest.java
@@ -3,6 +3,7 @@ package com.ceiba.api;
 import java.io.IOException;
 import java.time.LocalDateTime;
 import java.time.Month;
+import java.util.concurrent.TimeUnit;
 
 import org.junit.Assert;
 import org.junit.Test;
@@ -21,11 +22,20 @@ public class HolidayAbstractApiTest {
 	private HolidayAbstractApiAdapter abstractApi;
 	
 	@Test
-	public void esFestivoTest() throws IOException {
+	public void esFestivoTest() throws IOException, InterruptedException{
+		TimeUnit.SECONDS.sleep(1); //Esperar 1 segundo para consumir de nuevo la API
 		//arrange
 		LocalDateTime festivoColombiano = LocalDateTime.of(2020, Month.JULY, 20, 13, 1);
 		//act - assert
 		Assert.assertTrue(abstractApi.esFestivo(festivoColombiano));
 	}
-
+	
+	@Test
+	public void noEsFestivoTest() throws IOException, InterruptedException {		
+		TimeUnit.SECONDS.sleep(1); //Esperar 1 segundo para consumir de nuevo la API
+		//arrange
+		LocalDateTime festivoColombiano = LocalDateTime.of(2020, Month.JULY, 21, 13, 1);
+		//act - assert
+		Assert.assertFalse(abstractApi.esFestivo(festivoColombiano));
+	}
 }
